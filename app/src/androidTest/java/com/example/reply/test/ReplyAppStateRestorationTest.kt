0ee83881439b9eba7718cf8ecba1f6435c31d8f3
kura1aym <mukhtarkuralay249@mail.ru>
2023-11-18 07:03:01 +0600

    Test for a configuration change in the expanded screen

--- app/src/androidTest/java/com/example/reply/test/ReplyAppStateRestorationTest.kt
+++ app/src/androidTest/java/com/example/reply/test/ReplyAppStateRestorationTest.kt
@@ -62,4 +62,35 @@ class ReplyAppStateRestorationTest {
         ).assertExists()
     }
 
+    @Test
+    fun expandedDevice_selectedEmailEmailRetained_afterConfigChange() {
+        // Setup expanded window
+        val stateRestorationTester = StateRestorationTester(composeTestRule)
+        stateRestorationTester.setContent { ReplyApp(windowSize = WindowWidthSizeClass.Expanded) }
+
+        // Given third email is displayed
+        composeTestRule.onNodeWithText(
+            composeTestRule.activity.getString(LocalEmailsDataProvider.allEmails[2].body)
+        ).assertIsDisplayed()
+
+        // Select third email
+        composeTestRule.onNodeWithText(
+            composeTestRule.activity.getString(LocalEmailsDataProvider.allEmails[2].subject)
+        ).performClick()
+
+        // Verify that third email is displayed on the details screen
+        composeTestRule.onNodeWithTagForStringId(R.string.details_screen).onChildren()
+            .assertAny(hasAnyDescendant(hasText(
+                composeTestRule.activity.getString(LocalEmailsDataProvider.allEmails[2].body)))
+            )
+
+        // Simulate a config change
+        stateRestorationTester.emulateSavedInstanceStateRestore()
+
+        // Verify that third email is still displayed on the details screen
+        composeTestRule.onNodeWithTagForStringId(R.string.details_screen).onChildren()
+            .assertAny(hasAnyDescendant(hasText(
+                composeTestRule.activity.getString(LocalEmailsDataProvider.allEmails[2].body)))
+            )
+    }
 }
